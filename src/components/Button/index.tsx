5007b790a7dcc3b5be299eb3c97cea95da6f1c6c
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-08-15 14:05:24 -0300

    feat: adicionar funcao no componente de botao

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,8 +4,25 @@ type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
     name: string;
 }
 
+function sendMessageWhatsApp() {
+    // Guardar o numero de telefone
+    const phoneNumber = "5581995166980"
+
+    // Guardar a menssagem automatica
+    const message = "Olá, quero emagrecer com saúde usando o NutriBody agora!"
+    
+    // codificar a mensagem
+    const encodedMessage = encodeURIComponent(message)
+
+    // link para abir o whatsapp com a mensagem automatica
+    const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`
+
+    // Abrir em nova aba 
+    window.open(url, "_blank") 
+}
+
 export function Button({ name, ...rest}: Props) {
     return (
-        <button className={styles.container} {...rest}> {name} </button>
+        <button className={styles.container} onClick={sendMessageWhatsApp} {...rest}> {name} </button>
     )
 }
